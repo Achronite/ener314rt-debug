84c1f45f9cad8c81eae50ceb1691c33ff90b4868
Achronite <10363609+Achronite@users.noreply.github.com>
2020-01-11 19:07:26 +0000

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # ener314rt-debug
 Test program to diagnose issues with ener314rt and node
+
+## Check you have node installed, at least version 10
+``$ node -v``
+
+## To compile code
+``$ node-gyp rebuild``
+If you do not have node-gyp, please install using ``$ npm install -g node-gyp``
+
+## To run
+``$ node radio-test.js``
+
+# About
+
+Raise any bugs/queries here: https://github.com/Achronite/ener314rt-debug
+
+@Achronite - January 2020, uses energenie-ener314rt v0.3.2
---
+++ test.js
@@ -0,0 +1,49 @@
+//
+// This is a radio-test enabled test program for testing the radio code to diagnose issues for users
+//
+// You will need node.js v10+ or greater to execute
+//
+// run tests by:  
+//
+//  1) node radio-test.js
+//
+// @Achronite - January 2020
+//
+"use strict";
+
+var path = require('path');
+var switchState = false;
+
+// This uses the local module build, replace if using actual node.js module
+var ener314rt = require(path.join(__dirname, 'build/Release/ener314rt'));
+
+// monitor thread version in ener314rt uses a callback to return monitor messages directly (collected below), it needs the callback passing in
+function startMonitoringThread() {
+    ener314rt.openThingsReceiveThread(10000, (msg) => {
+        //console.log(`asyncOpenThingsReceive ret=${ret}`);
+        console.log(`radio-test-cb: received OTmsg=${msg}`);
+    });
+};
+
+
+// every 9 seconds toggle an OOK switch/teach message
+var intervalId = setInterval(() => {
+    let zone = 0;
+    let switchNum = 1;
+    switchState = !switchState;
+    let xmits = 20;
+    console.log(`radio-test: switching ${zone}:${switchNum}:${switchState}`)
+    var ret = ener314rt.ookSwitch(zone, switchNum, switchState, xmits);
+}, 9000);
+
+// Initialise
+console.log("radio-test: Initialising");
+var ret = ener314rt.initEner314rt(false);
+console.log(`radio-test: N-API radio_init returned ${ret}`);
+
+
+// every 30 seconds get the discovered deviceList
+var intervalId2 = setInterval(() => {
+    var devices = ener314rt.openThingsDeviceList(false);
+    console.log(`radio-test: deviceList=${devices}`);
+}, 30000);
