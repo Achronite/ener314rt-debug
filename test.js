01d91a57315b3aa7c707b9142b8d11925cd20e25
Achronite <10363609+Achronite@users.noreply.github.com>
2020-01-13 20:35:18 +0000

    Added monitor thread and quit if cannot initialise radio

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 //
-// This is a radio-test enabled test program for testing the radio code to diagnose issues for users
+// This is a test.js enabled test program for testing the radio code to diagnose issues for users
 //
 // You will need node.js v10+ or greater to execute
 //
@@ -21,7 +21,7 @@ var ener314rt = require(path.join(__dirname, 'build/Release/ener314rt'));
 function startMonitoringThread() {
     ener314rt.openThingsReceiveThread(10000, (msg) => {
         //console.log(`asyncOpenThingsReceive ret=${ret}`);
-        console.log(`radio-test-cb: received OTmsg=${msg}`);
+        console.log(`test.js-cb: received OTmsg=${msg}`);
     });
 };
 
@@ -32,18 +32,38 @@ var intervalId = setInterval(() => {
     let switchNum = 1;
     switchState = !switchState;
     let xmits = 20;
-    console.log(`radio-test: switching ${zone}:${switchNum}:${switchState}`)
+    console.log(`test.js: switching ${zone}:${switchNum}:${switchState}`)
     var ret = ener314rt.ookSwitch(zone, switchNum, switchState, xmits);
 }, 14000);
 
 // every 30 seconds get the discovered deviceList
 var intervalId2 = setInterval(() => {
     var devices = ener314rt.openThingsDeviceList(false);
-    console.log(`radio-test: deviceList=${devices}`);
+    console.log(`test.js: deviceList=${devices}`);
 }, 30000);
 
+// Capture ^C, stop monitoring
+process.on('SIGINT', function () {
+    console.log("test.js: Caught interrupt signal, stopping...");
+    ener314rt.stopMonitoring();
+
+    // Allow time for monitor thread to complete after timeout and close properly, do this as a cb to not block main event loop
+    setTimeout(function () {
+        console.log("test.js: finalizing close");
+        ener314rt.closeEner314rt();
+        process.exit();
+    },10000);
+});
 
 // Initialise radio adaptor
-console.log("radio-test: Initialising");
+console.log("test.js: Initialising board");
 var ret = ener314rt.initEner314rt(false);
-console.log(`radio-test: N-API radio_init returned ${ret}`);
+if (ret != 0) {
+    console.log(`test.js: ERROR: Cannot initialise ENER314-RT board, status=${ret}`);
+    console.log("test.js: Quitting");
+    process.exit();
+} else {
+    // start monitoring thread
+    console.log("test.js: Starting monitoring...");
+    startMonitoringThread();
+}
